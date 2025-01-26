4d51f4c8443da090dfad7d3791afbafea96f29b1
Josip Vojak <josipvojak@gmail.com>
2025-01-26 15:32:09 +0100

    Calculated number of PR, commits, pull requests and new issues based on preset distribution.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,87 @@
-use std::error::{self, Error};
-
-use octocrab::params::repos::forks::Sort;
+use dotenvy::dotenv;
+use rand::Rng;
+use std::env;
 
 const GIT_OWNER: &str = "spamserv";
 const GIT_REPO: &str = "gitspam";
+const GITHUB_PERSONAL_ACCESS_TOKEN: &str = "GITHUB_PERSONAL_ACCESS_TOKEN";
+
+#[derive(Debug)]
+struct ActivityDistributionMatrix {
+    commits: f64,
+    pull_requests: f64,
+    code_reviews: f64,
+    issues: f64,
+    daily_activities: DailyActivity 
+}
+
+#[derive(Debug, Clone)]
+struct DailyActivity {
+    low: u16,
+    high: u16
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    // Get repo
-    let repo = octocrab::instance()
-        .repos(GIT_OWNER, GIT_REPO)
-        .list_contributors()
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    let daily_activities = DailyActivity {
+        low: 2,
+        high: 27
+    };
+
+    let mut rng = rand::thread_rng();
+    let num_tasks = rng.gen_range(daily_activities.low..=daily_activities.high);
+    println!("Total tasks for the day: {}", num_tasks);
+
+    let activity_distribution_matrix = ActivityDistributionMatrix {
+        commits: 0.55,
+        pull_requests: 0.08,
+        code_reviews: 0.20,
+        issues: 0.17,
+        daily_activities: daily_activities.clone()
+    };
+
+    let mut total_activity_distribution = ActivityDistributionMatrix {
+        commits: (activity_distribution_matrix.commits * num_tasks as f64).round(),
+        pull_requests: (activity_distribution_matrix.pull_requests * num_tasks as f64).round(),
+        code_reviews: (activity_distribution_matrix.code_reviews * num_tasks as f64).round(),
+        issues: (activity_distribution_matrix.issues * num_tasks as f64).round(),
+        daily_activities: daily_activities.clone()
+    };
+
+    let total_calculated= (total_activity_distribution.commits + total_activity_distribution.pull_requests + total_activity_distribution.issues + total_activity_distribution.code_reviews) as u16;
+
+    let diff: i16 = num_tasks as i16 - total_calculated as i16;
+    if diff != 0 {
+        total_activity_distribution.commits += diff as f64
+    }
+
+    println!("{:?}", total_activity_distribution);
+
+    let octocrab = octocrab::Octocrab::builder()
+        .personal_token(
+            env::var(GITHUB_PERSONAL_ACCESS_TOKEN)
+                .to_owned()
+                .expect("Missing GITHUB_PERSONAL_ACCESS_TOKEN."),
+        )
+        .build()?;
+
+    let page = octocrab
+        .current()
+        .list_repos_for_authenticated_user()
+        .visibility("private")
         .send()
         .await?;
+
+    let repo = octocrab.repos(GIT_OWNER, GIT_REPO).list_branches().send().await?;
+
+    println!("{:?}", repo);
+    // Get repo
+    // let repo = octocrab
+    //     .repos(GIT_OWNER, GIT_REPO)
+    //     .list_contributors()
+    //     .send()
+    //     .await?;
 
     Ok(())
 
