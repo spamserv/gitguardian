b0a536c0986dfd594066dcdd2f5afbccc0d26ee3
Josip Vojak <josipvojak@gmail.com>
2025-01-26 14:41:23 +0100

    List repos.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
-fn main() {
+use std::error::{self, Error};
+
+use octocrab::params::repos::forks::Sort;
+
+const GIT_OWNER: &str = "spamserv";
+const GIT_REPO: &str = "gitspam";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    // Get repo
+    let repo = octocrab::instance()
+        .repos(GIT_OWNER, GIT_REPO)
+        .list_contributors()
+        .send()
+        .await?;
+
+    Ok(())
+
+    //format!("{:?}", repo);
     // Get random configuration based on inputs
 
     // Create commits
