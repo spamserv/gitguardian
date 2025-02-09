a7b860733b4c6a011f5e570745f70389c2a61ed1
Josip Vojak <josipvojak@gmail.com>
2025-02-09 15:12:57 +0100

    Added GitManager struct definition and implementation functions.

---
+++ src/git_manager/manager.rs
@@ -0,0 +1,27 @@
+use crate::config::activity_distribution::ActivityDistributionMatrix;
+
+pub struct GitManager {
+    activity_distribution: ActivityDistributionMatrix,
+}
+
+impl GitManager {
+    fn new(activity: ActivityDistributionMatrix) {
+        
+    }
+
+    fn create_commit() {
+
+    }
+
+    fn create_issue() {
+
+    }
+
+    fn create_branch() {
+
+    }
+
+    fn create_pull_request() {
+        
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod config;
 pub mod constants;
 pub mod git_models;
+pub mod git_manager;
