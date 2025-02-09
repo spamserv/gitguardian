43170d2b0b8c937d2d2d4eba0c3d2b321a361499
Josip Vojak <josipvojak@gmail.com>
2025-02-09 15:49:18 +0100

    Refactored create_branch and create_pull_requests functions.

--- src/git_manager/manager.rs
+++ src/git_manager/manager.rs
@@ -3,7 +3,13 @@ use std::env;
 use base64::{engine::general_purpose, Engine};
 use octocrab::{Error, Octocrab};
 
-use crate::{config::activity_distribution::ActivityDistributionMatrix, constants::github, git_models::git_models::{CreateRef, CreateReviewRequest, UpdateFileRequest, UpdateFileResponse, UpdateRepo}};
+use crate::{
+    config::activity_distribution::ActivityDistributionMatrix,
+    constants::github,
+    git_models::git_models::{
+        CreateRef, CreateReviewRequest, UpdateFileRequest, UpdateFileResponse, UpdateRepo,
+    },
+};
 
 pub struct GitManager {
     octocrab: Octocrab,
@@ -13,7 +19,12 @@ pub struct GitManager {
 
 impl GitManager {
     pub async fn new(activity: ActivityDistributionMatrix) -> Result<GitManager, Error> {
-        let readme_url = format!("/repos/{}/{}/contents/{}", github::GIT_OWNER, github::GIT_REPO, github::README_FILE_PATH);
+        let readme_url = format!(
+            "/repos/{}/{}/contents/{}",
+            github::GIT_OWNER,
+            github::GIT_REPO,
+            github::README_FILE_PATH
+        );
         let octocrab = octocrab::Octocrab::builder()
             .personal_token(
                 env::var(github::GITHUB_PERSONAL_ACCESS_TOKEN)
@@ -21,54 +32,57 @@ impl GitManager {
                     .expect("Missing GITHUB_PERSONAL_ACCESS_TOKEN."),
             )
             .build()?;
-        
+
         let manager = GitManager {
             readme_url,
             activity_distribution: activity,
-            octocrab
+            octocrab,
         };
         Ok(manager)
     }
 
     pub async fn enable_branch_autodelete(&self) -> Result<(), Error> {
-
         // 0. Update repo settings to enable auto-delete of a merged branch
         let repo_url = format!("/repos/{}/{}", github::GIT_OWNER, github::GIT_REPO);
         let update_settings = UpdateRepo {
             delete_branch_on_merge: true,
         };
-        let _updated_repo: serde_json::Value = self.octocrab.patch(repo_url, Some(&update_settings)).await?;
+        let _updated_repo: serde_json::Value = self
+            .octocrab
+            .patch(repo_url, Some(&update_settings))
+            .await?;
 
         Ok(())
     }
 
-    pub async fn create_commits(&self) -> Result<(),  Box<dyn std::error::Error>> {
+    pub async fn create_commits(&self) -> Result<(), Box<dyn std::error::Error>> {
         // println!("Repository settings updated: {:#}", updated_repo);
-        
+
         for commit_index in 0..self.activity_distribution.commits as u16 {
-            let readme_content = self.octocrab
-                    .repos(github::GIT_OWNER, github::GIT_REPO)
-                    .get_content()
-                    .path(github::README_FILE_PATH)
-                    .send()
-                    .await?;
+            let readme_content = self
+                .octocrab
+                .repos(github::GIT_OWNER, github::GIT_REPO)
+                .get_content()
+                .path(github::README_FILE_PATH)
+                .send()
+                .await?;
             let file_data = readme_content.items.get(0);
             let sha = readme_content.items.get(0).map(|i| i.sha.as_str());
             let message = format!("docs: update README.md {}", commit_index);
             let update_body = UpdateFileRequest {
-                    message: &message,
-                    content: general_purpose::STANDARD.encode("Updated README content!"),
-                    sha,                         // if Some(...) => update; if None => create
-                    branch: Some(github::GITHUB_BRANCH), // change to "master" or other if needed
-                };
+                message: &message,
+                content: general_purpose::STANDARD.encode("Updated README content!"),
+                sha,                                 // if Some(...) => update; if None => create
+                branch: Some(github::GITHUB_BRANCH), // change to "master" or other if needed
+            };
             self.octocrab
-                    .put::<UpdateFileResponse, _, _>(self.readme_url.clone(), Some(&update_body))
-                    .await?;
+                .put::<UpdateFileResponse, _, _>(self.readme_url.clone(), Some(&update_body))
+                .await?;
         }
         Ok(())
     }
 
-    pub async fn create_issues(&self) -> Result<(), Box<dyn std::error::Error>>{
+    pub async fn create_issues(&self) -> Result<(), Box<dyn std::error::Error>> {
         for issue_index in 0..self.activity_distribution.issues as u16 {
             // 2. Create issues
             let title = format!("{} #{}", github::GITHUB_ISSUE_NAME, issue_index);
@@ -82,7 +96,26 @@ impl GitManager {
         Ok(())
     }
 
-    fn create_branch() {}
+    async fn create_branch(
+        &self,
+        base_sha: &str,
+        branch_name: &str,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        // 1. Create branch
+        let ref_ = format!("refs/heads/{}", branch_name);
+        let create_ref_body = CreateRef {
+            ref_,
+            sha: base_sha.clone().to_owned(),
+        };
+
+        let post_ref_url = format!("/repos/{}/{}/git/refs", github::GIT_OWNER, github::GIT_REPO);
+        let _create_branch_response: serde_json::Value = self
+            .octocrab
+            .post::<CreateRef, _>(post_ref_url, Some(&create_ref_body))
+            .await?;
+
+        Ok(())
+    }
 
     pub async fn create_pull_requests(&self) -> Result<(), Box<dyn std::error::Error>> {
         /*
@@ -97,7 +130,8 @@ impl GitManager {
         */
 
         for pull_request_index in 0..self.activity_distribution.issues as u16 {
-            let readme_content = self.octocrab
+            let readme_content = self
+                .octocrab
                 .repos(github::GIT_OWNER, github::GIT_REPO)
                 .get_content()
                 .path(github::README_FILE_PATH)
@@ -108,24 +142,23 @@ impl GitManager {
             let sha = readme_content.items.get(0).map(|i| i.sha.as_str());
 
             // 0. Get base branch SHA
-            let get_ref_url = format!("/repos/{}/{}/git/ref/heads/{}", github::GIT_OWNER, github::GIT_REPO, github::GITHUB_BRANCH);
-            let base_ref_value: serde_json::Value = self.octocrab.get(get_ref_url, None::<&()>).await?;
+            let get_ref_url = format!(
+                "/repos/{}/{}/git/ref/heads/{}",
+                github::GIT_OWNER,
+                github::GIT_REPO,
+                github::GITHUB_BRANCH
+            );
+            let base_ref_value: serde_json::Value =
+                self.octocrab.get(get_ref_url, None::<&()>).await?;
             let base_sha = base_ref_value["object"]["sha"]
                 .as_str()
                 .ok_or("Could not extract base branch SHA")?
                 .to_string();
 
-            // 1. Create branch
-            let pull_request_branch_name = format!("{}-{}", github::GITHUB_PULL_REQUEST_BRANCH, offset);
-            let ref_ = format!("refs/heads/{}", pull_request_branch_name);
-            let create_ref_body = CreateRef {
-                ref_,
-                sha: base_sha.clone(),
-            };
+            let pull_request_branch_name =
+                format!("{}-{}", github::GITHUB_PULL_REQUEST_BRANCH, offset);
 
-            let post_ref_url = format!("/repos/{}/{}/git/refs", github::GIT_OWNER, github::GIT_REPO);
-            let _create_branch_response: serde_json::Value = self.octocrab
-                .post::<CreateRef, _>(post_ref_url, Some(&create_ref_body))
+            self.create_branch(&base_sha, &pull_request_branch_name)
                 .await?;
             //println!("Create branch response: {:#}", create_branch_response);
 
@@ -134,7 +167,7 @@ impl GitManager {
             let update_body = UpdateFileRequest {
                 message: &message,
                 content: general_purpose::STANDARD.encode("Pull Request: Updated README content!"),
-                sha,                                      // if Some(...) => update; if None => create
+                sha, // if Some(...) => update; if None => create
                 branch: Some(&pull_request_branch_name), // change to "master" or other if needed
             };
 
@@ -149,7 +182,8 @@ impl GitManager {
                 chrono::offset::Local::now(),
                 github::GITHUB_PULL_REQUEST_TITLE
             );
-            let pull_request = self.octocrab
+            let pull_request = self
+                .octocrab
                 .pulls(github::GIT_OWNER, github::GIT_REPO)
                 .create(
                     pull_request_title,
@@ -176,14 +210,18 @@ impl GitManager {
             };
             let review_request_url = format!(
                 "/repos/{}/{}/pulls/{}/reviews",
-                github::GIT_OWNER, github::GIT_REPO, pull_request.number
+                github::GIT_OWNER,
+                github::GIT_REPO,
+                pull_request.number
             );
-            let _review_request_response: serde_json::Value = self.octocrab
+            let _review_request_response: serde_json::Value = self
+                .octocrab
                 .post::<CreateReviewRequest, _>(review_request_url, Some(&review_request))
                 .await?;
 
             // 5. Merge pull request
-            let merge_response = self.octocrab
+            let merge_response = self
+                .octocrab
                 .pulls(github::GIT_OWNER, github::GIT_REPO)
                 .merge(pull_request.number)
                 .send()
